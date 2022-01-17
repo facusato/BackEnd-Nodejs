b7bd81f5cc424ecb97a94fc225ae62a6a46c0e6b
facundo cusato <facundocusato12@gmail.com>
2022-01-17 15:21:35 -0300

    Adding files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+
+//creamos el servidor
+const app = express();
+
+//conectamos a la bd
+conectarDB();
+
+
+app.use(express.json());
+
+app.use('/api/productos', require('./routes/producto'));
+
+app.listen(4000,()=> {
+
+    console.log('El servidor esta corriendo perfecto')
+})
